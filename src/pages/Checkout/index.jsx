2e4ed536db3ed02c2e7ae587367b7dd28fa9e99a
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-05-26 00:41:28 +0700

    add title on checkout

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 import Checkout from "../../components/templates/Checkout";
 import "./style.css";
@@ -6,6 +7,9 @@ import "./style.css";
 export default function CheckOut() {
   return (
     <div>
+      <Helmet>
+        <title>Tuku - Checkout</title>
+      </Helmet>
       <Checkout />
     </div>
   );
